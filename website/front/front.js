e534b8625d161731c724457b75c609787cf278aa
garyxu <xutianbian@qq.com>
2017-08-23 18:07:15 +0800

    vue-router

--- website/front/front.js
+++ website/front/front.js
@@ -3,5 +3,10 @@ require.config({
         "jquery" : ["../components/jquery/jquery-3.2.1.min"],
         "bootstrap" : ["../components/bootstrap-3.3.7/js/bootstrap.min"],
         "holder" : ["../components/bootstrap-3.3.7/assets/js/vendor/holder.min.js"]
-    }
+    },
+	shim: {　
+		'jquery': {
+			exports: 'jquery'
+		}　
+	}
 })
